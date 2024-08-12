7c5f464c8e6ddcb68204c7693f4a80c537af5666
unknown <sabinalopchan321@gmail.com>
2024-08-12 09:37:31 +0545

    updated

---
+++ connection.php
@@ -0,0 +1,19 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$db_name = "olympics";
+$port = 3307; // Specify the port number
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $db_name, $port);
+
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+echo "Connected successfully";
+
+// Close the connection
+// mysqli_close($conn);
+?>
