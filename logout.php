9a6e4eef31c3a1b6022bd88a0b074206ef157e8e
unknown <sabinalopchan321@gmail.com>
2024-08-12 11:28:40 +0545

    udit

--- logout.php
+++ logout.php
@@ -1,6 +1,15 @@
 <?php  
 session_start();
-session_destroy();
+include "connection.php";
+
+if (isset($_SESSION['username'])) {
+    $username = $_SESSION['username'];
+
+    // Log the logout action
+    $action = "Logout";
+    $log_query = "INSERT INTO audit_logs (username, action) VALUES ('$username', '$action')";
+    mysqli_query($conn, $log_query);
+}session_destroy();
 header("location:index.php");
 exit(); // Ensure that no further code is executed after the redirect
 ?>
